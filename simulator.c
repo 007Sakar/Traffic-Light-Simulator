8948a3c7132e5600768248f556f9b5b9ce5bc91a
Sakar <sakardhimal94@gmail.com>
2025-12-24 22:12:27 +0545

    continuation of simulator

--- simulator.c
+++ simulator.c
@@ -7,3 +7,11 @@ int readPriority(int *);
 int qA=0, qB=0, qC=0, qD=0, qP=0;
 
 void serve(char road, int *q) {
+    void serveRoad(char name, Queue* q) {
+    printf("\nGREEN LIGHT → Road %c\n", name);
+    int v = dequeue(q);
+    if (v > 0) {
+        printf("Serving %d vehicles (Time = %d sec)\n", v, v * T);
+        sleep(v * T);
+    }
+}
