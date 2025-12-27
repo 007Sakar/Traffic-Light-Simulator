394c1e0a29f6bb65b8bfdb0dfd8db47115a41a79
Sakar <sakardhimal94@gmail.com>
2025-12-27 18:33:33 +0545

    done some minute changes on recievers and adders and also made the file sdlc3 compatable by solving linking issue

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Traffic-Light-Simulator
 this project the simulation of traffic lights of the streets to maintin and manage the traffic
+to run simulator.exe type
+.\simulator.exe
+gcc simulator.c -o simulator -Iinclude -Llib -lSDL3
+
+
+Terminal 1 – Generators
+./traffic_generator
+./traffic_generator2
+./traffic_generator3
+
+
+gcc simulator.c -o simulator -lSDL3
+./simulator
--- traffic_generator.c
+++ traffic_generator.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <time.h>
+#include <windows.h>
 
 int main() {
-    FILE *fp;
     srand(time(NULL));
+    int vehicle_id = 1;
+
+    printf("Generator started. Creating vehicles...\n");
 
     while (1) {
-        fp = fopen("vehicles.data", "a");
-        fprintf(fp, "A %d\n", rand() % 3 + 1);
-        fprintf(fp, "B %d\n", rand() % 3 + 1);
+        FILE *fp = fopen("vehicle.data", "a");
+        if (!fp) {
+            printf("Error: Cannot open vehicle.data\n");
+            return 1;
+        }
+
+        int road = rand() % 4;
+        int lane = rand() % 3;
+
+        fprintf(fp, "%d %d %d\n", road, lane, vehicle_id);
         fclose(fp);
-        sleep(2);
+
+        printf("Created: Vehicle %d on Road %d, Lane %d\n", vehicle_id, road, lane);
+        vehicle_id++;
+
+        Sleep(500); // 0.5 seconds
     }
+    return 0;
 }
